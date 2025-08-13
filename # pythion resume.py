d19cc7618c960d51152483253fd2af2cb473b352
eduarmarin <donsecun@gmail.com>
2025-08-13 11:31:51 +0200

    install command: py -m pip install camelcase"

--- # pythion resume.py
+++ # pythion resume.py
@@ -12,3 +12,10 @@ resume= {
     "binary" : ["bytes", "bytearray", "memoryview"],
     "none": "NoneType"
 }
+
+# print the resume
+for key, value in resume.items():
+    if isinstance(value, list):
+        print(f"{key.capitalize()}: {', '.join(value)}")
+    else:
+        print(f"{key.capitalize()}: {value}")
