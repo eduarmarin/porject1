e31fd8de9248a09083c584dae992870448984eee
eduarmarin <donsecun@gmail.com>
2025-08-13 11:30:11 +0200

    first commit

---
+++ # pythion resume.py
@@ -0,0 +1,14 @@
+# pythion resume
+
+# data types using a dictionary:
+
+resume= {
+    "text": "Python Resume",
+    "numeric": ["int", "float", "complex"],
+    "sequence" : ["list", "tuple", "range"],
+    "mapping" : "dict",
+    "set" : ["set", "Frozenset"],
+    "boolean" : ["True", "False"],
+    "binary" : ["bytes", "bytearray", "memoryview"],
+    "none": "NoneType"
+}
